f022a9ef554602471773cf62c9d06a1f9134625f
sule11 <smkargin11@gmail.com>
2022-08-27 08:27:21 -0500

    updated Agile and Selenium classes

--- Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/Agile.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/Agile.java
@@ -2,6 +2,6 @@ package com.cydeo.stereotype_annotation;
 
 public class Agile {
     public void getTeachingHours(){
-        System.out.println("Total teaching hours :  250");
+        System.out.println("Total teaching hours :  15");
     }
 }
