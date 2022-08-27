120610749c8265b72b82ad5387a276a6f59f3cd8
sule11 <smkargin11@gmail.com>
2022-08-27 07:05:31 -0500

    added CydeoApp class

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,13 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
+
+
+    }
+}
