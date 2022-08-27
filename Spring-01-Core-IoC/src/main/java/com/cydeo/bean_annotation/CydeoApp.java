e4f9925c85b81e298965f2c2fa26a78faae37009
sule11 <smkargin11@gmail.com>
2022-08-27 07:16:00 -0500

    updated CydeoApp class

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CydeoApp {
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
 
 
     }
