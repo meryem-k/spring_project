02ebd46db16cb51069949355cfcd9b519987267e
sule11 <smkargin11@gmail.com>
2022-08-28 22:22:59 -0500

    Abstraction

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    //@Autowired
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+
+    }
+}
