dc5003faea459fdea8341fed78083e7422a20bcd
sule11 <smkargin11@gmail.com>
2022-09-03 11:48:28 -0500

    Mentor Task

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,33 @@
+package com.cydeo.controller;
+
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MentorController {
+
+    @RequestMapping("/mentor")
+    public String mentorPage(Model model){
+
+        List<Mentor> mentorList = new ArrayList<>();
+
+        mentorList.add(new Mentor("Mike","Smith",25,Gender.MALE));
+        mentorList.add(new Mentor("Emily","Johnson",22,Gender.FEMALE));
+        mentorList.add(new Mentor("Tom","Walsh",65,Gender.MALE));
+
+        model.addAttribute("mentor", mentorList);
+
+        return "/mentor/mentor-list";
+    }
+
+
+
+
+
+}
