01d236ce5e0df83abe6e7f026f2403301034c4b9
sule11 <smkargin11@gmail.com>
2022-10-23 07:57:19 -0500

    implementing one to many and many to many relationship between tables

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/test/java/com/cydeo/spring12ormonetomanymanytomany/Spring12OrmOneToManyManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring12ormonetomanymanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12OrmOneToManyManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
