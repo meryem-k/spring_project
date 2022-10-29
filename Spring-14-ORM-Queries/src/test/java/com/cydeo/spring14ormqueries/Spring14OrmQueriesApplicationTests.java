f810bf91b8d66bd9e30649322e244251ccb2eb02
sule11 <smkargin11@gmail.com>
2022-10-29 11:54:48 -0500

    Queries

---
+++ Spring-14-ORM-Queries/src/test/java/com/cydeo/spring14ormqueries/Spring14OrmQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring14ormqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring14OrmQueriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
